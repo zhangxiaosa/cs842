e0522c7e9ac7e7a88886611800a18a39505f0fe9
zhangxiaosa <hustzmx@gmail.com>
2022-11-19 14:42:25 -0500

    generate gcov

--- collect_coverage/collect.py
+++ collect_coverage/collect.py
@@ -3,36 +3,47 @@ import subprocess
 import glob
 
 def execute(cmd):
-    subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    proc.wait()
     #subprocess.Popen(cmd, shell=True)
 
-def cleanup(path):
-    print("starting: cleanup")
-    for f in glob.glob(path + '/**/*.gcov', recursive=True):
-        execute("rm %s" % f)
-        print("removed %s" % f)
-    for f in glob.glob(path + '/**/*.gcda', recursive=True):
+def cleanup(path, extension):
+    print("starting: cleanup %s in %s" % (extension, path))
+    for f in glob.glob(path + '/**/*.%s' % extension, recursive=True):
         execute("rm %s" % f)
         print("removed %s" % f)
 
-def run_testsuites():
-    pass
+def find_files(path, extension):
+    return glob.glob(path + '/**/*.%s' % extension, recursive=True)
 
-def collect():
-    pass
 
 opt_level = "-O2"
-gcc_path = "../bin/gcc/"
-gcov_path = "../bin/gcov/"
+gcc_path = "../bin/gcc"
+gcc_flags = "-I /usr/local/include/csmith/"
+gcov_path = "../bin/gcov"
+gcov_flags = "-i"
 benchmark_path = "../benchmark/"
-coverage_path = "../gcc/"
-other_flags = "-I /usr/local/include/csmith/"
+gcda_result_path = "../gcc/"
+gcov_result_path = "./gcov_result/"
 
-benchmark = glob.glob(os.path.join(benchmark_path, '*.c'))
 
-cleanup(coverage_path)
+# cleanup gcda
+cleanup(gcda_result_path, "gcda")
 
+# run benchmark
+benchmark = find_files(benchmark_path, 'c')
 for test_case in benchmark:
     print("starting: ", test_case)
-    cmd = "%s %s %s %s" % (gcc_path, opt_level, other_flags, test_case)
+    cmd = "%s %s %s %s" % (gcc_path, opt_level, gcc_flags, test_case)
     execute(cmd)
+
+# cleanup gcov
+cleanup(gcov_result_path, "gcov")
+
+# generate gcov files
+gcda_files = find_files(gcda_result_path, "gcda")
+os.chdir(gcov_result_path)
+for gcda_file in gcda_files:
+    execute("%s %s %s" % (gcov_path, gcov_flags, gcda_file))
+os.chdir("..")
+
